bd94e3a7b1970e0bcf8db18349b344fc5757bc26
Ron de Groot <ron.de.groot@persgroep.nl>
2018-09-28 14:50:22 +0200

    Adding BaseEntity with ID for models

--- pet-clinic-data/src/main/java/nl/rondgrt/myperclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/nl/rondgrt/myperclinic/model/Pet.java
@@ -2,7 +2,7 @@ package nl.rondgrt.myperclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
--- pet-clinic-data/src/main/java/nl/rondgrt/myperclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/nl/rondgrt/myperclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package nl.rondgrt.myperclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
