0a2ea5a11f54eb1c47b453ba747804b6f5cc2a3c
Ron de Groot <ron.de.groot@persgroep.nl>
2018-09-30 13:22:30 +0200

    Refactoring to common CRUD interface

--- pet-clinic-data/src/main/java/nl/rondgrt/myperclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/nl/rondgrt/myperclinic/services/PetService.java
@@ -2,13 +2,6 @@ package nl.rondgrt.myperclinic.services;
 
 import nl.rondgrt.myperclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/nl/rondgrt/myperclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/nl/rondgrt/myperclinic/services/VetService.java
@@ -2,13 +2,6 @@ package nl.rondgrt.myperclinic.services;
 
 import nl.rondgrt.myperclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
