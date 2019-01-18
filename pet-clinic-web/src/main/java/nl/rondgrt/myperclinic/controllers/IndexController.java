8fc6d8466efee68625e2b3869f957cf5b66dc9db
Rondgrt <rondgrt@gmail.com>
2019-01-18 12:08:44 +0100

    Adding logging and jMeter test

--- pet-clinic-web/src/main/java/nl/rondgrt/myperclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/nl/rondgrt/myperclinic/controllers/IndexController.java
@@ -1,13 +1,18 @@
 package nl.rondgrt.myperclinic.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    Logger logger = LoggerFactory.getLogger(IndexController.class);
+
     @RequestMapping({"", "/", "index", "index.html"})
     public String index() {
+        logger.info("Request for homepage of myPetClinic");
         return "index";
     }
 
