fa850a1c31d5f06afe9fea62c296b05dc5003fa1
Ron de Groot <rondgrt@gmail.com>
2019-08-18 14:02:55 +0200

    Updated visit entity with JPA annotations

--- pet-clinic-data/src/main/java/nl/rondgrt/myperclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/nl/rondgrt/myperclinic/model/Speciality.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "specialities")
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
     @Column(name = "description")
